ce6a88d30b02e36cd0c100ebaac806310277f10b
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-12 00:19:15 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,11 +1,9 @@
 
 def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
   create_olympics_hashhash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
 end
 
 def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
   create_olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
   add_a_key_value_pair = {:Atlanta => "1996"}
   create_olympics_hash.merge(add_a_key_value_pair)
@@ -13,9 +11,6 @@ def add_a_key_value_pair
 end
 
 def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-  
    create_olympics_hash = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
   add_a_key_value_pair = {:Atlanta => "1996"}
   create_olympics_hash.merge(add_a_key_value_pair)
@@ -26,9 +21,6 @@ end
 end
 
 def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-  
  final_array = {:Atlanta => "1996", :Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
  final_array.each_key {|key| puts key.upcase}
   puts
